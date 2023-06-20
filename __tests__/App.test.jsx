9f6482954d5ed11e8a992987e489657a86de7b9d
Juntao Qiu <juntao.qiu@gmail.com>
2023-06-20 13:17:24 +1000

    build the app and run the tests

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -5,6 +5,15 @@ import '@testing-library/jest-dom/extend-expect';
 
 import App from '../src/components/App';
 
+global.fetch = jest.fn().mockImplementation(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([
+      { quote: "Quote 1", author: "Author 1" },
+      { quote: "Quote 2", author: "Author 2" },
+    ]),
+  })
+);
+
 test('renders welcome message', () => {
   render(<App />);
   expect(screen.getByText("Welcome to A React Application!")).toBeInTheDocument();
